1c490ff6b188a71303137efcbf6a1005deea57a6
Esbjörn Eriksson <esbjoern.eriksson@erasys.de>
2017-03-30 19:58:47 +0200

    Do a proper fetch

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-set -eu
+set -eux
 
 REPOSITORY_URL=$1
 BRANCH=$2
@@ -13,7 +13,7 @@ REPO_PATH="/repos/$(echo ${REPOSITORY_URL} | sed 's/[^a-zA-Z0-9]/-/g')"
 if ! [ -d ${REPO_PATH} ] ; then
   git clone -q --bare ${REPOSITORY_URL} ${REPO_PATH}
 else
-  git --git-dir=${REPO_PATH} fetch -q
+  git --git-dir=${REPO_PATH} fetch -q --prune origin refs/heads/*:refs/heads/*
 fi
 
 git --git-dir=${REPO_PATH} describe --abbrev=0 --tags --match "*.*.*" ${BRANCH}
